98fc3afb31fa3cad75f5e58f39df6e694881d79b
KunAttila69 <71005372+KunAttila69@users.noreply.github.com>
2022-09-28 17:12:45 +0200

    Delete sakk directory

--- sakk/script.js
+++
@@ -1,7 +0,0 @@
-function Main(sorok){
-    for (let sor = 0; sor < 8; sor++) {
-        for (let cella = 0; cella < 8; cella++) {
-            document.getElementById("sor"+sor).innerHTML += "<div class='cella "+sorok[sor][cella]+"' ></div>"
-        }
-    }
-}
